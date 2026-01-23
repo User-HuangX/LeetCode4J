b88ecb5f20c2df8a8212bff35f809315660d35e1
User-HuangX <3221834658@qq.com>
2026-01-23 16:16:31 +0800

    电话簿

--- src/main/java/com/werun/solution/backtracking/SolutionLetterCombinations.java
+++ src/main/java/com/werun/solution/backtracking/SolutionLetterCombinations.java
@@ -3,20 +3,22 @@ package com.werun.solution.backtracking;
 import java.util.*;
 
 public class SolutionLetterCombinations {
-    private static final Map<Character, Character[]> MAP = new HashMap<>(8);
-    static {
-        MAP.put('2', new Character[]{'a', 'b', 'c'});
-        MAP.put('3', new Character[]{'d', 'e', 'f'});
-        MAP.put('4', new Character[]{'g', 'h', 'i'});
-        MAP.put('5', new Character[]{'j', 'k', 'l'});
-        MAP.put('6', new Character[]{'m', 'n', 'o'});
-        MAP.put('7', new Character[]{'p', 'q', 'r', 's'});
-        MAP.put('8', new Character[]{'t', 'u', 'v'});
-        MAP.put('9', new Character[]{'w', 'x', 'y', 'z'});
-    }
+    private static final String[] MAPPING = new String[] {
+            "", //0
+            "", //1
+            "abc", //2
+            "edf",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
+    };
+
     public List<String> letterCombinations(String digits) {
         ArrayList<String> ans = new ArrayList<>();
-        backtracking(digits, new StringBuffer(),ans,0);
+        backtracking(digits, new StringBuffer(), ans, 0);
         return ans;
     }
 
@@ -27,11 +29,11 @@ public class SolutionLetterCombinations {
         }
         char[] charArray = digits.toCharArray();
         for (int i = start, charArrayLength = charArray.length; i < charArrayLength; i++) {
-            char c = charArray[i];
-            for (int j = 0; j < MAP.get(c).length; j++) {
-                path.append(MAP.get(c)[j]);
+            int num = charArray[i] - '0';
+            for (int j = 0; j < MAPPING[num].length(); j++) {
+                path.append(MAPPING[num].charAt(j));
                 backtracking(digits, path, ans, i + 1);
-                path.deleteCharAt(path.length()-1);
+                path.deleteCharAt(path.length() - 1);
             }
         }
     }
